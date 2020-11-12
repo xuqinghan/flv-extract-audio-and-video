09facb555864a54b8124f096d56212edef234d73
xuqinghan <xuqinghan@gmail.com>
2020-11-13 05:30:23 +0800

    parse header flvfile

--- extract_and_play.py
+++ extract_and_play.py
@@ -3,6 +3,7 @@
 
 同时出音频，视频帧，并回调而不是直接保存
 '''
+import struct
 
 AUDIO = 0x8
 VIDEO = 0x9
@@ -36,7 +37,28 @@ def bytes_to_int(bytes_string):
     '''
     return int.from_bytes(bytes_string, byteorder="big")
 
+def parse_flvfile_header(f_stream):
+    '''
+        FLV格式参考 https://blog.csdn.net/byxdaz/article/details/53993791
+            # 
+        struct unpack 用 'B' 转成 8bit的 short 然后才可以用 & 和 >> 得到值了
+        用bytes  反而难操作
+        已经写到了cnblog：https://www.cnblogs.com/xuanmanstein/p/13967320.html
+    '''
+    ##header 9 字节
+    chunk = f_stream.read(9)
+    # s 仍然是1个Bytes 不好处理，读成1个short然后才能位操作
+    #format_str = '>3sBsI'
+    format_str = '>3sBBI'
+    flv, version, stream_type_bits, len_header = struct.unpack(format_str, chunk)
 
+    has_audio =  (stream_type_bits & 0x7) >> 2
+    has_video = stream_type_bits &  0x1
+
+    return {'version': version,
+            'has_video': has_video,
+            'has_audio': has_audio, 
+            'len_header': len_header} #len_header 9 字节
 
 def parse_FLV_TAG1(f_tag):
     PreviousTagSize, *_ = struct.unpack('>I', f_tag.read(4))
@@ -70,14 +92,6 @@ class Parse:
         self._bytes_begin = 13 # 3 + 1 + 1 + 4 + 4
         self._audio_tag_header = None
 
-    # def header_assert(self):
-    #     '''
-    #     assert the flv header 
-    #     '''
-    #     assert(self._flv_data[0] == ord("F"))
-    #     assert(self._flv_data[1] == ord("L"))
-    #     assert(self._flv_data[2] == ord("V"))
-    #     assert(self._flv_data[3] == 1)# assert the flv version
 
     def calculate_audio_specific_config(self, bytes_string):
         '''
@@ -108,7 +122,7 @@ class Parse:
         '''
         #current = self._bytes_begin
         while self.flv_stream:
-
+            
             tag_type = self._flv_data[current]         
             tag_data_size = bytes_to_int(self._flv_data[current+1:current+4])
             tag_data = self._flv_data[current+11:current+11+tag_data_size]
@@ -123,3 +137,44 @@ class Parse:
             current += 11 + tag_data_size
             assert(bytes_to_int(self._flv_data[current:current+4]) == 11 + tag_data_size)
             current += 4
+
+def test_read_flvfile_header_without_struct():
+    '''不用struct'''
+    fname = './xb2_kos.flv'
+    f_stream = open(fname, 'rb')
+
+    data_bytes = f_stream.read(9)
+    flv = data_bytes[0:3]
+
+    print(flv)
+    version = data_bytes[3]
+    print('version', version)
+    print(len(data_bytes))
+    #从bytes里取出来了就是是int！
+    print(type(data_bytes[4]))
+    #转为str 'b' 补足8位
+    stream_type_bits = format(data_bytes[4], 'b').zfill(8)
+    print('stream_type_bits', stream_type_bits, type(stream_type_bits))
+    #按bit取出string，按二进制再转成int
+    has_audio = int(stream_type_bits[5], 2)
+    print('has_audio', has_audio)
+    has_video = int(stream_type_bits[7], 2)
+    print('has_video', has_video)
+    len_header = bytes_to_int(data_bytes[5:])
+    print('len_header', len_header)
+
+
+
+if __name__ == '__main__':
+    fname = './xb2_kos.flv'
+    f_stream = open(fname, 'rb')
+    #test_read_flvfile_header()
+    flvfile_header = parse_flvfile_header(f_stream)
+    print(flvfile_header)
+
+
+
+    #PreviousTagSize = bytes_to_int(data_bytes)
+    #print('PreviousTagSize', PreviousTagSize)
+
+    f_stream.close()
